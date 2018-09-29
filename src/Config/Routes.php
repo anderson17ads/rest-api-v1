94c7835f4cf849e1c46556ef916ed6b4a0add8aa
Anderson de Souza <anderson17ads@hotmail.com>
2018-09-29 18:33:59 -0300

    Forçando o content-type das rotas de usuários para retornar json

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -3,6 +3,17 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/usuarios', function (Request $request, Response $response) {
-	$usersController = new App\Controllers\UsersController;
-	$usersController->listar();
+	$usersController = new App\Controllers\UsersController($request, $response);
+
+	return $response
+		->withHeader('Content-Type', 'application/json')
+        ->write(json_encode($usersController->all()));
+});
+
+$app->get('/usuario/{id}', function (Request $request, Response $response) {
+	$usersController = new App\Controllers\UsersController($request, $response);
+
+	return $response
+		->withHeader('Content-Type', 'application/json')
+		->write(json_encode($usersController->get()));
 });
--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -5,13 +5,33 @@ use App\Config\Database;
 
 class UsersController
 {
-	public function listar()
+	public function __construct($request, $response)
+	{
+		$this->request  = $request;
+		$this->response = $response;
+	}
+
+	public function all()
 	{
 		try {
 			$db = new Database;
 			$stmt = $db->conn->query('SELECT * FROM usuarios');
 
-			echo json_encode($stmt->fetchAll(\PDO::FETCH_OBJ));
+			return $stmt->fetchAll(\PDO::FETCH_OBJ);
+		} catch (\PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public function get()
+	{
+		$id = $this->request->getAttribute('id');
+
+		try {
+			$db = new Database;
+			$stmt = $db->conn->query("SELECT * FROM usuarios WHERE id = {$id}");
+
+			return $stmt->fetchAll(\PDO::FETCH_OBJ);
 		} catch (\PDOException $e) {
 			echo $e->getMessage();
 		}
