53afa8ad00e760d323952dd2df4ef1f6f2d9bfea
Anderson de Souza <anderson17ads@hotmail.com>
2018-09-30 12:20:26 -0300

    Criando a edição e exclusão de usuário

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -10,10 +10,34 @@ $app->get('/usuarios', function (Request $request, Response $response) {
 		->write(json_encode($usersController->all()));
 });
 
-$app->get('/usuario/{id}', function (Request $request, Response $response) {
+$app->get('/usuarios/{id}', function (Request $request, Response $response) {
 	$usersController = new App\Controllers\UsersController($request);
 
 	return $response
 		->withHeader('Content-Type', 'application/json')
 		->write(json_encode($usersController->get()));
+});
+
+$app->post('/usuarios', function (Request $request, Response $response) {
+	$usersController = new App\Controllers\UsersController($request);
+
+	return $response
+		->withHeader('Content-Type', 'application/json')
+		->write(json_encode($usersController->add()));
+});
+
+$app->put('/usuarios/{id}', function (Request $request, Response $response) {
+	$usersController = new App\Controllers\UsersController($request);
+
+	return $response
+		->withHeader('Content-Type', 'application/json')
+		->write(json_encode($usersController->edit()));
+});
+
+$app->delete('/usuarios/{id}', function (Request $request, Response $response) {
+	$usersController = new App\Controllers\UsersController($request);
+
+	return $response
+		->withHeader('Content-Type', 'application/json')
+		->write(json_encode($usersController->delete()));
 });
--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -7,9 +7,14 @@ class UsersController
 {
 	public function __construct($request)
 	{
-		$this->request  = $request;
+		$this->request = $request;
 	}
 
+	/**
+	 * Recupera todos os usuários
+	 *
+	 * @return string JSON
+	 */
 	public function all()
 	{
 		try {
@@ -18,10 +23,15 @@ class UsersController
 
 			return $stmt->fetchAll(\PDO::FETCH_OBJ);
 		} catch (\PDOException $e) {
-			echo $e->getMessage();
+			echo '{"error: {"message" : '. $e->getMessage() .'}}';
 		}
 	}
 
+	/**
+	 * Recupera um usuário
+	 *
+	 * @return string JSON
+	 */
 	public function get()
 	{
 		$id = $this->request->getAttribute('id');
@@ -32,7 +42,85 @@ class UsersController
 
 			return $stmt->fetchAll(\PDO::FETCH_OBJ);
 		} catch (\PDOException $e) {
-			echo $e->getMessage();
+			echo '{"error: {"message" : '. $e->getMessage() .'}}';
+		}
+	}
+
+	/**
+	 * Adiciona um novo usuário
+	 *
+	 * @return string JSON
+	 */
+	public function add()
+	{
+		$nome 	  = $this->request->getParam('nome');
+		$telefone = $this->request->getParam('telefone');
+		$email 	  = $this->request->getParam('email');
+
+		try {
+			$db = new Database;
+			$stmt = $db->conn->prepare("INSERT INTO usuarios (nome, telefone, email) VALUES (:nome, :telefone, :email)");
+
+			$stmt->bindParam(':nome', $nome);
+			$stmt->bindParam(':telefone', $telefone);
+			$stmt->bindParam(':email', $email);
+
+			$stmt->execute();
+
+			echo '{"notice": {"message" : "Usuário adicionado com sucesso!"}}';
+		} catch (\PDOException $e) {
+			echo '{"error: {"message" : '. $e->getMessage() .'}}';
+		}
+	}
+
+	/**
+	 * Edita o usuário
+	 *
+	 * @return string JSON
+	 */
+	public function edit()
+	{
+		$id 	  = $this->request->getAttribute('id');
+		$nome 	  = $this->request->getParam('nome');
+		$telefone = $this->request->getParam('telefone');
+		$email 	  = $this->request->getParam('email');
+
+		$sql = "UPDATE usuarios SET nome = :nome, telefone = :telefone, email = :email WHERE id = {$id}";
+
+		try {
+			$db = new Database;
+			$stmt = $db->conn->prepare($sql);
+
+			$stmt->bindParam('nome', $nome);
+			$stmt->bindParam('telefone', $telefone);
+			$stmt->bindParam('email', $email);
+
+			$stmt->execute();
+
+			echo '{"notice" : {"message" : "Usuário alterado com sucesso!"}}';
+		} catch (\PDOException $e) {
+			echo '{"error" : {"message" : '. $e->getMessage() .'}}';
+		}
+	}
+
+	/**
+	 * Deleta o usuário
+	 *
+	 * @return string JSON
+	 */
+	public function delete()
+	{
+		$id = $this->request->getAttribute('id');
+
+		try {
+			$db = new Database;
+			$stmt = $db->conn->prepare("DELETE FROM usuarios WHERE id = {$id}");
+			
+			$stmt->execute();
+
+			echo '{"notice" : {"message" : "Usuário deletado com sucesso!"}}';
+		} catch (\PDOException $e) {
+			echo '{"error" : {"message" : '. $e->getMessage() .'}}';
 		}
 	}
 }
